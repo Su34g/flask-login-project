17dc33d4bb8107fbced2a5ff5e55a1e36241e3b3
suyog <162372175+Su34g@users.noreply.github.com>
2026-02-26 00:34:54 +0530

    Refactor database connection to use environment variable
    Updated database connection to use environment variable for DATABASE_URL.

--- login_project/server.py
+++ login_project/server.py
@@ -1,16 +1,11 @@
 from flask import Flask, render_template, request
 import psycopg2
+import os
 
 app = Flask(__name__)
 
-conn = psycopg2.connect(
-    database="login_db",
-    user="postgres",
-    password="suyog",
-    host="localhost",
-    port="5432"
-)
-
+# Read database URL from environment variable
+conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
 cur = conn.cursor()
 
 @app.route("/", methods=["GET", "POST"])
@@ -19,7 +14,10 @@ def home():
         username = request.form["username"]
         password = request.form["password"]
 
-        cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+        cur.execute(
+            "INSERT INTO users (username, password) VALUES (%s, %s)",
+            (username, password)
+        )
         conn.commit()
 
     return render_template("index.html")
